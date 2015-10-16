e8a0a4c4ef8fb2c569f763af71e8856a0e5a4668
sndnvaps <sndnvaps@gmail.com>
2015-10-16 21:07:17 +0800

    first commit
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

---
+++ DHT22_GpioOneWire/Dht22.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace DHT22_GpioOneWire
+{
+    class Dht22
+    {
+        GpioPin dht22pin;
+        private const int DHT22_PIN = 24;
+
+       Dht22(GpioPin pin)
+        {
+
+        }
+
+    }
+
+
+}
---
+++ DHT22_GpioOneWire/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Gpio;
+
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace DHT22_GpioOneWire
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault(); 
+            //show an error if there is no GPIO Controller
+            if (gpio == null)
+            {
+                dht22pin = null;
+                GpioStatus.Text = "There is no GPIO Controller on this devicd.";
+                return;
+            }
+
+            dht22pin = gpio.OpenPin(DHT22_PIN);
+
+            dht22pin.SetDriveMode(GpioPinDriveMode.Input);
+        }
+
+        private GpioPin dht22pin;
+        private const int DHT22_PIN = 24; //定义为24 
+    }
+    
+}
